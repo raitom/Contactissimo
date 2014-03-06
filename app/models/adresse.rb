dab82122bc3a703dcb9eef7647dd538935fee251
Thomas Geselle <raitomm@gmail.com>
2014-03-06 12:15:38 +0100

    Evenements

--- app/models/adresse.rb
+++ app/models/adresse.rb
@@ -1,3 +1,6 @@
 class Adresse < ActiveRecord::Base
   belongs_to :adressable, :polymorphic => true
+
+  validates_presence_of :adresse, :ville, :code_postal, :adressable_id, :adressable_type
+  validates :code_postal, length: {is: 5}
 end
